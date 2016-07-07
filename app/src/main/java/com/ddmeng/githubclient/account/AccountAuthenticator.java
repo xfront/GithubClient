370888d680bf51bf809745e0d0f64b671977a4b9
Dandan MENG <mengdandanno1@163.com>
2016-07-07 13:48:24 +0800

    implement getAuthToken() method in AccountAuthenticator

--- app/src/main/java/com/ddmeng/githubclient/account/AccountAuthenticator.java
+++ app/src/main/java/com/ddmeng/githubclient/account/AccountAuthenticator.java
@@ -9,7 +9,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.ddmeng.githubclient.utils.LogUtils;
+
 public class AccountAuthenticator extends AbstractAccountAuthenticator {
+
+    private static final String TAG = AccountAuthenticator.class.getSimpleName();
+    public static final String AUTH_TOKEN_TYPE = "auth_token_type";
 
     private Context context;
 
@@ -26,6 +31,7 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
     @Override
     public Bundle addAccount(AccountAuthenticatorResponse accountAuthenticatorResponse,
                              String s, String s1, String[] strings, Bundle bundle) throws NetworkErrorException {
+        LogUtils.footPrint();
         final Intent intent = new Intent(context, AuthenticatorActivity.class);
         intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, accountAuthenticatorResponse);
         final Bundle returnedBundle = new Bundle();
@@ -39,8 +45,41 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
     }
 
     @Override
-    public Bundle getAuthToken(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String s, Bundle bundle) throws NetworkErrorException {
-        return null;
+    public Bundle getAuthToken(AccountAuthenticatorResponse accountAuthenticatorResponse, Account account, String authTokenType, Bundle bundle) throws NetworkErrorException {
+        LogUtils.footPrint();
+        LogUtils.i(TAG, "authTokenType: " + authTokenType);
+        // If the caller requested an authToken type we don't support, then
+        // return an error
+        if (!authTokenType.equals(AUTH_TOKEN_TYPE)) {
+            final Bundle result = new Bundle();
+            result.putString(AccountManager.KEY_ERROR_MESSAGE, "invalid authTokenType");
+            return result;
+        }
+        // Extract the username and password from the Account Manager, and ask
+        // the server for an appropriate AuthToken.
+//        final AccountManager am = AccountManager.get(context);
+//        final String password = am.getPassword(account);// permission required, we don't do that
+//        if (password != null) {
+//            final String authToken = NetworkUtilities.authenticate(account.name, password);
+//            if (!TextUtils.isEmpty(authToken)) {
+//                final Bundle result = new Bundle();
+//                result.putString(AccountManager.KEY_ACCOUNT_NAME, account.name);
+//                result.putString(AccountManager.KEY_ACCOUNT_TYPE, context.getString(R.string.account_type));
+//                result.putString(AccountManager.KEY_AUTHTOKEN, authToken);
+//                return result;
+//            }
+//        }
+
+        // If we get here, then we couldn't access the user's password - so we
+        // need to re-prompt them for their credentials. We do that by creating
+        // an intent to display our AuthenticatorActivity panel.
+        final Intent intent = new Intent(context, AuthenticatorActivity.class);
+        intent.putExtra(AuthenticatorActivity.PARAM_USERNAME, account.name);
+        intent.putExtra(AuthenticatorActivity.PARAM_AUTH_TOKEN_TYPE, authTokenType);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE, accountAuthenticatorResponse);
+        final Bundle result = new Bundle();
+        result.putParcelable(AccountManager.KEY_INTENT, intent);
+        return result;
     }
 
     @Override
--- app/src/main/java/com/ddmeng/githubclient/account/AuthenticatorActivity.java
+++ app/src/main/java/com/ddmeng/githubclient/account/AuthenticatorActivity.java
@@ -7,6 +7,11 @@ import com.ddmeng.githubclient.R;
 
 public class AuthenticatorActivity extends AccountAuthenticatorActivity {
 
+    /** The Intent extra to store username. */
+    public static final String PARAM_USERNAME = "username";
+    /** The Intent extra to store username. */
+    public static final String PARAM_AUTH_TOKEN_TYPE = "auth_token_type";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
